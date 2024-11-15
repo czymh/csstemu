ef0bb730130ede42989f749ec18644ad120e8586
Zhao Chen <chyiru@sjtu.edu.cn>
2024-11-15 23:13:34 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
+packages = ['CEmulator',
+            'CEmulator/emulator',
+            'CEmulator/GaussianProcess',
+            'CEmulator/hankl',]
 
 setup(
     name='CEmulator',
@@ -6,7 +11,7 @@ setup(
     author='Zhao Chen',
     author_email='chyiru@sjtu.edu.cn',
     description='A python package for CSST cosmological emulator, which can predict various cosmological statistics within miliseconds.',
-    packages=find_packages(),
+    packages=packages,
     url='https://github.com/czymh/csstemu',
     install_requires=[
         'numpy',
@@ -17,4 +22,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    package_dir={'euclidemu2': 'CEmulator'},
+    package_data={'CEmulator': ['data/*']},
+    include_package_data=True,
 )
