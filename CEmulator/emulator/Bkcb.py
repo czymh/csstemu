add HMF_CEmulator [need more test]; z-distribution normalization bug fix for WeakLensing get_lensing_kernel; TODO: need more stable and fast method for the comoving distance interpolator

CEmulator/emulator/Bkcb.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.interpolate import RectBivariateSpline
 from ..GaussianProcess.GaussianProcess import GaussianProcessRegressor, Constant, RBF 
-from ..utils import cosmoNorm, cosmoNormLarge, data_path, zlists, check_k, check_z, MyStandardScaler
+from ..utils import cosmoNorm, cosmoNormLarge, data_path, zlists, checkdata, check_z, MyStandardScaler
 
 class Bkcb_gp:
     zlists = zlists 
@@ -50,7 +50,7 @@ class Bkcb_gp:
         k : float or array-like, wavenumber [h/Mpc]
         '''
         z = check_z(self.zlists, z)
-        k = check_k(self.klist, k)
+        k = checkdata(self.klist, k, dname='wavenumber')
         if self.NormBeforeGP:
             Normcosmo = self.paramSS.transform(self.ncosmo)
         else:
@@ -119,7 +119,7 @@ class Bkcb_halofit_gp:
         k : float or array-like, wavenumber [h/Mpc]
         '''
         z = check_z(self.zlists, z)
-        k = check_k(self.klist, k)
+        k = checkdata(self.klist, k, dname='wavenumber')
         if self.NormBeforeGP:
             Normcosmo = self.paramSS.transform(self.ncosmo)
         else:
@@ -188,7 +188,7 @@ class Bkcb_hmcode2020_gp:
         k : float or array-like, wavenumber [h/Mpc]
         '''
         z = check_z(self.zlists, z)
-        k = check_k(self.klist, k)
+        k = checkdata(self.klist, k, dname='wavenumber')
         if self.NormBeforeGP:
             Normcosmo = self.paramSS.transform(self.ncosmo)
         else:
@@ -256,7 +256,7 @@ class Bkcb_lin2hmcode_gp:
         k : float or array-like, wavenumber [h/Mpc]
         '''
         z = check_z(self.zlists, z)
-        k = check_k(self.klist,  k)
+        k = checkdata(self.klist,  k, dname='wavenumber')
         if self.NormBeforeGP:
             Normcosmo = self.paramSS.transform(self.ncosmo)
         else:

CEmulator/emulator/PkLin.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.interpolate import RectBivariateSpline
 from ..GaussianProcess.GaussianProcess import GaussianProcessRegressor, Constant, RBF 
-from ..utils import data_path, zlists, check_k, check_z, MyStandardScaler, cosmoNormLarge
+from ..utils import data_path, zlists, checkdata, check_z, MyStandardScaler, cosmoNormLarge
 
 class PkcbLin_gp:
     zlists = zlists 
@@ -47,7 +47,7 @@ class PkcbLin_gp:
         k : float or array-like, wavenumber [h/Mpc]
         '''
         z = check_z(self.zlists, z)
-        k = check_k(self.klist, k)
+        k = checkdata(self.klist, k, dname='wavenumber')
         if self.NormBeforeGP:
             Normcosmo = self.paramSS.transform(self.ncosmo)
         else:
@@ -115,7 +115,7 @@ class Pknn_cbLin_gp:
         k : float or array-like, wavenumber [h/Mpc]
         '''
         z = check_z(self.zlists, z)
-        k = check_k(self.klist, k)
+        k = checkdata(self.klist, k, dname='wavenumber')
         if self.NormBeforeGP:
             Normcosmo = self.paramSS.transform(self.ncosmo)
         else:

CEmulator/emulator/Pkhm.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.interpolate import RectBivariateSpline
 from ..GaussianProcess.GaussianProcess import GaussianProcessRegressor, Constant, Matern
-from ..utils import cosmoNorm, data_path, zlists, check_k, check_z
+from ..utils import cosmoNorm, data_path, zlists, checkdata, check_z
 
 class PkhmMassBin_gp:
     zstart = 7
@@ -48,7 +48,7 @@ class PkhmMassBin_gp:
         r : float or array-like, wavenumber [Mpc/h]
         '''
         z = check_z(self.zlists_reduce, z)
-        k = check_k(self.klist,  k)
+        k = checkdata(self.klist,  k, dname='wavenumber')
         
         ## Gaussian Process Regression
         Bkpred = np.zeros((self.nmassbin, self.nvec))

CEmulator/emulator/TkNuNncdm.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.interpolate import RectBivariateSpline
 from ..GaussianProcess.GaussianProcess import GaussianProcessRegressor, Constant, RBF 
-from ..utils import cosmoNormLarge, data_path, zlists, check_k, check_z, MyStandardScaler
+from ..utils import cosmoNormLarge, data_path, zlists, checkdata, check_z, MyStandardScaler
 
 class Tkbase_gp:
     zlists = zlists
@@ -51,7 +51,7 @@ class Tkbase_gp:
         k : float or array-like, wavenumber [h/Mpc]
         '''
         z = check_z(self.zlists, z)
-        k = check_k(self.klist, k)
+        k = checkdata(self.klist, k, dname='wavenumber')
         if self.NormBeforeGP:
             Normcosmo = self.paramSS.transform(self.ncosmo)
         else:

CEmulator/emulator/Xihm.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.interpolate import RectBivariateSpline
 from ..GaussianProcess.GaussianProcess import GaussianProcessRegressor, Constant, Matern
-from ..utils import cosmoNorm, data_path, zlists, check_k, check_z
+from ..utils import cosmoNorm, data_path, zlists, check_z
 
 class XihmMassBin_gp:
     zstart = 7

CEmulator/emulator/Ximm.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.interpolate import RectBivariateSpline
 from ..GaussianProcess.GaussianProcess import GaussianProcessRegressor, Constant, Matern
-from ..utils import cosmoNorm, data_path, zlists, check_r, check_z, MyStandardScaler
+from ..utils import cosmoNorm, data_path, zlists, check_z, MyStandardScaler
 
 class Ximm_cb_gp:
     zlists = zlists 

CEmulator/utils.py
@@ -33,29 +33,17 @@ def check_z(zlists=zlists,z=None):
         print('Predicting redshifts (sorted):', zinput)
     return zinput
     
-def check_k(klists, k=None):
-    if k is None:
-        raise ValueError('Please provide the wavenumber k [h/Mpc].')
-    kinput = np.atleast_1d(k)
-    if np.any(kinput < klists[0]-1e-8):
-        raise ValueError('kinput min=%.8f is smaller than the lower limit %.8f.'%(np.min(kinput), klists[0]))
-    if np.any(kinput > klists[-1]+1e-8):
-        raise ValueError('kinput max=%.8f is larger than the upper limit %.8f.'%(np.max(kinput), klists[-1]))
-    if not np.all(np.diff(kinput) > 0.0):
-        raise ValueError('k must be strictly increasing!')
-    return kinput
-
-def check_r(rlists, r=None):
-    if r is None:
-        raise ValueError('Please provide the wavenumber r [Mpc/h].')
-    rinput = np.atleast_1d(r)
-    if np.any(rinput < rlists[0]):
-        raise ValueError('r min=%.8f is smaller than the lower limit %.8f.'%(np.min(r), rlists[0]))
-    if np.any(rinput > rlists[-1]):
-        raise ValueError('r max=%.8f is larger than the upper limit %.8f.'%(np.max(r), rlists[-1]))
-    if not np.all(np.diff(rinput) > 0.0):
-        raise ValueError('r must be strictly increasing!')
-    return rinput
+def checkdata(dlists, d=None, dname='wavenumber'):
+    if d is None:
+        raise ValueError('Please provide the %s.'%dname)
+    dinput = np.atleast_1d(d)
+    if np.any(dinput < dlists[0]-1e-8):
+        raise ValueError('The input of [%s] min=%.8f is smaller than the lower limit %.8f.'%(dname, np.min(dinput), dlists[0]))
+    if np.any(dinput > dlists[-1]+1e-8):
+        raise ValueError('The input of [%s] max=%.8f is larger than the upper limit %.8f.'%(dname, np.max(dinput), dlists[-1]))
+    if not np.all(np.diff(dinput) > 0.0):
+        raise ValueError('%s must be strictly increasing!'%dname)
+    return dinput
 
 def NormCosmo(cosmologies, param_names, param_limits):
     '''