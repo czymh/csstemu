687941e84c58ff5a3d0c8da5ed800572a510f57a
Zhao Chen <chyiru@sjtu.edu.cn>
2026-07-09 14:29:28 +0800

    compatibility for simps import from higher version scipy

--- CEmulator/cosmology.py
+++ CEmulator/cosmology.py
@@ -1,4 +1,9 @@
 import numpy as np
+from scipy.integrate import quad
+try:
+    from scipy.integrate import simps
+except ImportError:
+    from scipy.integrate import simpson as simps
 from scipy.interpolate import interp1d
 from .utils import data_path
 
